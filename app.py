c48f67af4056665b1345f44b50d1dc0d1909991f
victor <32772604+victorbahlangene@users.noreply.github.com>
2022-05-10 14:25:24 +0300

    Update app.py
    adding try catch

--- app.py
+++ app.py
@@ -17,17 +17,22 @@ st.header("Hair Braid Classification")
 path = Path()
 learn_inf = load_learner(path/'hair.pkl')
 
-# File uploader #
-uploaded_file = st.file_uploader("Upload Files", type=['png', 'jpeg', 'jpg'])
-if uploaded_file is not None:
-    image_file = PILImage.create((uploaded_file))
+try:
+   # File uploader #
+    uploaded_file = st.file_uploader("Upload Files", type=['png', 'jpeg', 'jpg'])
+    if uploaded_file is not None:
+        image_file = PILImage.create((uploaded_file))
 
-# Display uploaded image #
-st.image(image_file.to_thumb(400, 400), caption='Uploaded Image')
+    # Display uploaded image #
+    st.image(image_file.to_thumb(400, 400), caption='Uploaded Image')
 
-# make prediction and display result #
-if st.button('Classify'):
-    pred, pred_idx, probs = learn_inf.predict(image_file)
-    st.write(f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}')
-else:
-    st.write(f'Click the button to classify')
+    # make prediction and display result #
+    if st.button('Classify'):
+        pred, pred_idx, probs = learn_inf.predict(image_file)
+        st.write(f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}')
+    else:
+        st.write(f'Click the button to classify') 
+    
+except NameError:
+    # error will only be visible in the comand line #
+    print("Name error is caught")
